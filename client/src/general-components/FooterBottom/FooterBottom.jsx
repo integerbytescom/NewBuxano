49c3900426a8afef21ccff97da3477618e9b294c
kdubasov <cergocergo41@gmail.com>
2022-12-09 12:10:32 +0300

    add BetsGroup.jsx and FooterBottom.jsx

--- client/src/general-components/FooterBottom/FooterBottom.jsx
+++ client/src/general-components/FooterBottom/FooterBottom.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import "./FooterBottom.css";
+import {Link} from "react-router-dom";
+import {FooterBottomLinks} from "./FooterBottomLinks.js";
 
 const FooterBottom = () => {
     return (
         <footer className={"FooterBottom"}>
-
+            {
+                FooterBottomLinks.map((link,ids) => (
+                    <Link key={ids} to={link.link}>{link.text}</Link>
+                ))
+            }
         </footer>
     );
 };
---
+++ client/src/general-components/FooterBottom/FooterBottomLinks.js
@@ -0,0 +1,34 @@
+export const FooterBottomLinks = [
+    {
+        text:"© BUXANO 2020",
+        link:"/",
+    },
+    {
+        text:"Homepage",
+        link:"/",
+    },
+    {
+        text:"API Documentation",
+        link:"/",
+    },
+    {
+        text:"Fees",
+        link:"/",
+    },
+    {
+        text:"Terms & Conditions",
+        link:"/",
+    },
+    {
+        text:"Risk Disclaimer",
+        link:"/",
+    },
+    {
+        text:"Blog",
+        link:"/",
+    },
+    {
+        text:"FAQ",
+        link:"/",
+    },
+]
